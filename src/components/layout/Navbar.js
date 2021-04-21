42c14313a9f446298e2ba386afd46ce9a69ca3fb
Fredrik000 <52358347+Fredrik000@users.noreply.github.com>
2021-04-21 14:52:34 +0200

    More components, React router etc

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,20 +1,29 @@
-import React from "react";
-import PropTypes from "prop-types";
-import UserItem from "../users/UserItem";
+import React from 'react';
+import PropTypes from 'prop-types';
+import UserItem from '../users/UserItem';
+import { Link } from 'react-router-dom';
 
 const Navbar = ({ icon, title }) => {
-    return (
-      <nav className='navbar bg-primary'>
-        <h1>
-          <i className={icon} /> {title}
-        </h1>
-      </nav>
-    );
-  }
+  return (
+    <nav className='navbar bg-primary'>
+      <h1>
+        <i className={icon} /> {title}
+      </h1>
+      <ul>
+        <li>
+          <Link to='/'>Home</Link>
+        </li>
+        <li>
+          <Link to='/about'>About</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
 Navbar.defaultProps = {
-  title: "Github Finder",
-  icon: "fab fa-github",
+  title: 'Github Finder',
+  icon: 'fab fa-github',
 };
 
 Navbar.propTypes = {
@@ -24,6 +33,6 @@ Navbar.propTypes = {
 
 UserItem.protoTypes = {
   user: PropTypes.object.isRequired,
-}
+};
 
 export default Navbar;
